23d905d2e2c1d69906eb247c7262185456583eb6
Abi Castro <abixcastro@gmail.com>
2023-06-15 15:17:38 -0700

    Initial commit

---
+++ template/src/services/wallets/mirrorNodeClient.js
@@ -0,0 +1,11 @@
+export class MirrorNodeClient {
+  constructor(networkConfig) {
+    this.url = networkConfig.mirrorNodeUrl;
+  }
+
+  async getAccountInfo(accountId) {
+    const accountInfo = await fetch(`${this.url}/api/v1/accounts/${accountId}`, { method: "GET" });
+    const accountInfoJson = await accountInfo.json();
+    return accountInfoJson;
+  }
+}
